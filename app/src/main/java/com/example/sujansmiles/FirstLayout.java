f83af71852beb65e3752b6874b6c9ca349ee77f6
SujanMh <sujanmh@noveltytechnology.com>
2025-02-19 23:25:08 +0545

    Initial controller for first-layout

---
+++ app/src/main/java/com/example/sujansmiles/FirstLayout.java
@@ -0,0 +1,19 @@
+package com.example.sujansmiles;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class FirstLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+    }
+}
