e9af545318479c536d230cfd8d0b381eea64f30c
SujanMh <sujanmh@noveltytechnology.com>
2025-04-24 09:11:29 +0545

    added the helper methods for update and delete

--- app/src/main/java/com/example/sujansmiles/subjects/SqLiteHelper.java
+++ app/src/main/java/com/example/sujansmiles/subjects/SqLiteHelper.java
@@ -27,16 +27,16 @@ public class SqLiteHelper extends SQLiteOpenHelper {
 
     /**
      * CREATE TABLE subject_tbl (
-     *      id INTEGER PRIMARY KEY AUTOINCREMENT,
-     *      name TEXT,
-     *      color INTEGER
+     * id INTEGER PRIMARY KEY AUTOINCREMENT,
+     * name TEXT,
+     * color INTEGER
      * )
      *
      * @param db The database.
      */
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createTableQuery = "CREATE TABLE "+ TABLE_NAME + "(\n" +
+        String createTableQuery = "CREATE TABLE " + TABLE_NAME + "(\n" +
                 COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
                 COLUMN_NAME + " TEXT,\n" +
                 COLUMN_COLOR + " INTEGER )";
@@ -56,7 +56,7 @@ public class SqLiteHelper extends SQLiteOpenHelper {
      * U: Update
      * D: Delete
      */
-    public void create(String name, int color){
+    public void create(String name, int color) {
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues contentValues = new ContentValues();
@@ -69,18 +69,19 @@ public class SqLiteHelper extends SQLiteOpenHelper {
 
     /**
      * SELECT * FROM <table-name>
+     *
      * @return
      */
-    public ArrayList<Subject> read(){
+    public ArrayList<Subject> read() {
         ArrayList<Subject> subjects = new ArrayList<>();
 
         SQLiteDatabase db = this.getReadableDatabase();
 
-        String selectQuery = "SELECT * FROM "+ TABLE_NAME;
-        Cursor cursor = db.rawQuery(selectQuery,null);
+        String selectQuery = "SELECT * FROM " + TABLE_NAME;
+        Cursor cursor = db.rawQuery(selectQuery, null);
 
-        if(cursor.moveToFirst()){
-            do{
+        if (cursor.moveToFirst()) {
+            do {
                 int id = cursor.getInt(cursor.getColumnIndex(COLUMN_ID));
                 String name = cursor.getString(cursor.getColumnIndex(COLUMN_NAME));
                 int color = cursor.getInt(cursor.getColumnIndex(COLUMN_COLOR));
@@ -89,9 +90,35 @@ public class SqLiteHelper extends SQLiteOpenHelper {
 
                 subjects.add(subject);
 
-            }while (cursor.moveToNext());
+            } while (cursor.moveToNext());
         }
 
         return subjects;
+    }
+
+    public void update(int id, String name, int color) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_NAME, name);
+        contentValues.put(COLUMN_COLOR, color);
+
+        db.update(
+                TABLE_NAME,
+                contentValues,
+                COLUMN_ID + " = ?", new String[]{String.valueOf(id)}
+        );
+
+        db.close();
+    }
+
+    public void delete(int id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(
+                TABLE_NAME,
+                COLUMN_ID + " = ?", new String[]{String.valueOf(id)}
+        );
+        db.close();
     }
 }
